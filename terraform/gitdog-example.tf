387f929a139502b86ef55825751fba6ba36b26de
BEMELON <hgdkim2@naver.com>
2023-05-17 15:52:28 +0900

    chore: 더 이상 S3 업로드를 통해서 Lambda가 배포되지 않음  - Local에서 .zip 파일을 만들어 해당 파일으로 직접 lambda에 배포되도록 설정

--- terraform/gitdog-example.tf
+++ terraform/gitdog-example.tf
@@ -1,13 +1,28 @@
-// 1. Archive the source code
+// 1. Archive the lambda function
 data "archive_file" "zip_gitdog_example" {
   type        = "zip"
-  source_file = "${path.root}/../gitdog-example/lambda.py"
+  source_dir  = "${path.root}/../gitdog-example/"
   output_path = "${path.root}/../gitdog-example.zip"
 }
 
-// 2. Upload the archive to S3
-resource "aws_s3_object" "s3_gitdog_example" {
-  bucket = aws_s3_bucket.gitdog-bucket.id
-  key    = "gitdog-example.zip"
-  source = data.archive_file.zip_gitdog_example.output_path
+// 2. Create the lambda function
+resource "aws_lambda_function" "gitdog_example" {
+  filename         = data.archive_file.zip_gitdog_example.output_path
+  function_name    = "gitdog-example"
+  role             = aws_iam_role.iam_for_lambda.arn
+  handler          = "index.lambda_handler"
+  source_code_hash = data.archive_file.zip_gitdog_example.output_base64sha256
+  runtime          = "python3.10"
+
+
+  timeout     = 60
+  memory_size = 128
+  publish     = true
+  tags = {
+    Name = "gitdog-example"
+  }
+
+  ephemeral_storage {
+    size = 1024 # Min 512 MB and the Max 10240 MB
+  }
 }
--- terraform/s3.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_s3_bucket" "gitdog-bucket" {
-  bucket        = "mju-gitdog-s3-bucket"
-  force_destroy = true
-}
