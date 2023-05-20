754c9f9915c7bc66ed2efb1cc99dc5ee252addc1
BEMELON <hgdkim2@naver.com>
2023-05-20 20:21:21 +0900

    refactor: terraofm 파일 리팩토링  - config.tf > main.tf : Provider 설정  - iam.tf : AWS iam들 정리

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,19 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-
-  required_version = ">= 1.2.0"
-}
-
-provider "aws" {
-  # Asia/Seoul
-  region = "ap-northeast-2"
-}
-
 data "aws_iam_policy_document" "assume_role" {
   statement {
     effect = "Allow"
