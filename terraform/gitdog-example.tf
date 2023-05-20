9e36a85dd14313a2eb91bc7f2f4bd7d61eecad86
BEMELON <hgdkim2@naver.com>
2023-05-20 19:54:42 +0900

    chore: requirements.txt to lambda layer  - 외부 패키지를 requirements.txt에 정리하하면, terraform에서 layer를 구축하여 업로드드하는 기능

--- terraform/gitdog-example.tf
+++ terraform/gitdog-example.tf
@@ -1,11 +1,35 @@
-// 1. Archive the lambda function
+// 1. install Python requirements
+resource "null_resource" "install_python_dependencies" {
+  provisioner "local-exec" {
+    command = "pip install -r ${path.root}/../gitdog-example/requirements.txt -t ${path.root}/../gitdog-example/"
+  }
+}
+
+// 2. Archive the lambda function
 data "archive_file" "zip_gitdog_example" {
   type        = "zip"
   source_dir  = "${path.root}/../gitdog-example/"
   output_path = "${path.root}/../gitdog-example.zip"
 }
 
-// 2. Create the lambda function
+// 3. Archive the lambda function layer
+data "archive_file" "layer_gitdog_example" {
+  depends_on  = [null_resource.install_python_dependencies]
+  type        = "zip"
+  source_dir  = "${path.root}/../gitdog-example/"
+  output_path = "${path.root}/../gitdog-example-layer.zip"
+}
+
+// 4. Create the lambda function layer
+resource "aws_lambda_layer_version" "layer_gitdog_example" {
+  layer_name          = "gitdog-example-layer"
+  filename            = data.archive_file.layer_gitdog_example.output_path
+  source_code_hash    = data.archive_file.layer_gitdog_example.output_base64sha256
+  compatible_runtimes = ["python3.10"]
+  description         = "gitdog-example-layer"
+}
+
+// 5. Create the lambda function
 resource "aws_lambda_function" "gitdog_example" {
   filename         = data.archive_file.zip_gitdog_example.output_path
   function_name    = "gitdog-example"
@@ -13,6 +37,7 @@ resource "aws_lambda_function" "gitdog_example" {
   handler          = "index.lambda_handler"
   source_code_hash = data.archive_file.zip_gitdog_example.output_base64sha256
   runtime          = "python3.10"
+  layers           = [aws_lambda_layer_version.layer_gitdog_example.arn]
 
 
   timeout     = 60
